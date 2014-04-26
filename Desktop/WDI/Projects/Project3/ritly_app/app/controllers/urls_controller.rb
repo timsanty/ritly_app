f2ff5442d15cfe6f4bb9f4dd72adc43b9884af15
timsanty <timsanty@me.com>
2014-04-25 22:16:37 -0700

    initial functionality done

---
+++ Desktop/WDI/Projects/Project3/ritly_app/app/controllers/urls_controller.rb
@@ -0,0 +1,30 @@
+class UrlsController < ApplicationController
+
+	def index
+		@url = Url.new
+		@random = SecureRandom.urlsafe_base64(4)
+	end	
+
+	def create
+		@url = Url.create url_params
+		redirect_to url_path(@url)
+	end
+
+	def show
+		@url = Url.find(params[:id])
+	end
+
+	def go
+		url = Url.find_by random_string: params[:random_string]
+		redirect_to "https://#{url[:link]}"
+	end
+
+	def preview
+		#take original URL and create preview
+	end
+
+	private
+		def url_params
+			params.require(:url).permit(:link, :random_string)
+		end
+end
---
+++ Desktop/WDI/Projects/Project3/ritly_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RitlyApp::Application.config.secret_key_base = 'b7a22de62bedb2f540a29eb4df1c2ee91b072e8008a92ddb94ee9dd99731e45edc4f97d703348110b5556abf9c7e109235c29e12651277a746bfa5f0be4c4ed9'
